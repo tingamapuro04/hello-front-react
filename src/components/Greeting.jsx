50def1b8c8215898cab939374209894aa21fddaa
tingamapuro04 <alponcemobutu100@gmail.com>
2023-02-22 10:14:12 +0300

    Fetch api data

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const Greeting = () => {
+  const data = useSelector((state) => state.apidata.data);
+
+  return (
+    <div>
+      we are now connected to
+      {data && <h1>{data.language}</h1>}
+      {data && <p>{data.body}</p>}
+    </div>
+  );
+};
+
+export default Greeting;
---
+++ src/features/dataasync/dataSlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getApiData = createAsyncThunk("messages/getApiData", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/messages");
+  const data = await response.json();
+  return data;
+});
+
+const apiSlice = createSlice({
+  name: "data",
+  initialState: {
+    data: null,
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getApiData.pending, (state) => {
+        // state.status = 'loading';
+        const newState = state;
+        newState.status = "loading";
+      })
+      .addCase(getApiData.fulfilled, (state, action) => {
+        // state.status = 'succeeded';
+        // state.data = action.payload;
+        const newState = state;
+        newState.status = "succeeded";
+        newState.data = action.payload;
+      })
+      .addCase(getApiData.rejected, (state, action) => {
+        // state.status = 'failed';
+        // state.error = action.error.message;
+        const newState = state;
+        newState.status = "failed";
+        newState.error = action.error.message;
+      });
+  },
+});
+
+const localApiData = apiSlice.reducer;
+export default localApiData;
